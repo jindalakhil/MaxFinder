45f0b24cc414597ee4fa629defd21e77a89cc9b7
jindalakhil <akhiljindal222@gmail.com>
2020-10-08 23:53:47 +0530

    [Akhil]Added: Float Max Finder

--- src/main/java/com/capg/MaxFinder.java
+++ src/main/java/com/capg/MaxFinder.java
@@ -2,6 +2,14 @@ package com.capg;
 
 public class MaxFinder {
 	public static void main(String[] args) {
+		Integer a =1;
+		Integer b= 2;
+		Integer c =3;
+		System.out.println("Maximum Integer is "+ findMax(a, b, c));
+		Float x = 23.12f;
+		Float y = 1231.123123f;
+		Float z = 1312.1231f;
+		System.out.println("Maximum Float Number is "+ findMax(x, y, z));
 		
 	}
 	public static Integer findMax(Integer a, Integer b, Integer c) {
@@ -11,6 +19,15 @@ public class MaxFinder {
 			max = b;
 		if(c.compareTo(max)>0)
 			max = c;
+		return max;
+	}
+	
+	public static Float findMax(Float x,Float y,Float z) {
+		Float max=x;
+		if(y.compareTo(max)>0)
+			max=y;
+		if(z.compareTo(max)>0)
+			max=z;
 		return max;
 	}
 }
--- src/test/java/com/capg/MaxFinderTester.java
+++ src/test/java/com/capg/MaxFinderTester.java
@@ -13,20 +13,37 @@ public class MaxFinderTester {
 	}
 
 	@Test
-	public void test1_1() {
+	public void test_When_1_Is_Mazimum() {
 		Integer a =15;
 		assertEquals(a,maxfinder.findMax(15,7,6));
 	}
 	
 	@Test
-	public void test1_2() {
+	public void test_When_2_Is_Mazimum() {
 		Integer a =15;
 		assertEquals(a,maxfinder.findMax(7,15,6));
 	}
 	@Test
-	public void test1_3() {
+	public void test_When_3_Is_Mazimum() {
 		Integer a =15;
 		assertEquals(a,maxfinder.findMax(7,6,15));
+	}
+	
+	@Test
+	public void test_When_1_Float_Is_Mazimum() {
+		Float a =15.23f;
+		assertEquals(a,maxfinder.findMax(15.23f,7.123f,15.15f));
+	}
+	
+	@Test
+	public void test_When_2_Float_Is_Mazimum() {
+		Float a =15.23f;
+		assertEquals(a,maxfinder.findMax(15.1f,15.23f,7.23f));
+	}
+	@Test
+	public void test_When_3_Float_Is_Mazimum() {
+		Float a =15.23f;
+		assertEquals(a,maxfinder.findMax(15.1f,7.123f,15.23f));
 	}
 	
 }
