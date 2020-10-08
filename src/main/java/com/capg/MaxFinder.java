443463c3eda06a88c9d7c6acee3c3d49ced0459a
jindalakhil <akhiljindal222@gmail.com>
2020-10-09 00:01:16 +0530

    [Akhil]Added: String Max Finder

--- src/main/java/com/capg/MaxFinder.java
+++ src/main/java/com/capg/MaxFinder.java
@@ -10,7 +10,10 @@ public class MaxFinder {
 		Float y = 1231.123123f;
 		Float z = 1312.1231f;
 		System.out.println("Maximum Float Number is "+ findMax(x, y, z));
-		
+		String e = "akhil";
+		String f = "jindal";
+		String g = "name";
+		System.out.println("Maximum String is "+ findMax(e, f, g));
 	}
 	public static Integer findMax(Integer a, Integer b, Integer c) {
 		Integer max = a;
@@ -28,6 +31,15 @@ public class MaxFinder {
 			max=y;
 		if(z.compareTo(max)>0)
 			max=z;
+		return max;
+	}
+	
+	public static String findMax(String a,String b,String c) {
+		String max= a;
+		if(b.compareTo(max)>0)
+			max=new String(b);
+		if(c.compareTo(max)>0)
+			max=new String(c);
 		return max;
 	}
 }
--- src/test/java/com/capg/MaxFinderTester.java
+++ src/test/java/com/capg/MaxFinderTester.java
@@ -46,4 +46,21 @@ public class MaxFinderTester {
 		assertEquals(a,maxfinder.findMax(15.1f,7.123f,15.23f));
 	}
 	
+	@Test
+	public void test_When_1_String_Is_Mazimum() {
+		String a ="Kumar";
+		assertEquals(a,maxfinder.findMax("Kumar", "Jindal", "Akhil"));
+	}
+	
+	@Test
+	public void test_When_2_String_Is_Mazimum() {
+		String a ="Kumar";
+		assertEquals(a,maxfinder.findMax("Akhil", "Kumar", "Jindal"));
+	}
+	@Test
+	public void test_When_3_String_Is_Mazimum() {
+		String a ="Kumar";
+		assertEquals(a,maxfinder.findMax("Akhil", "Jindal", "Kumar"));
+	}
+	
 }
